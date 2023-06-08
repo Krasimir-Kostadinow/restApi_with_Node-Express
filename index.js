19f82b658440dd5a8755659c4effeb9067a90458
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2023-06-08 16:53:30 +0300

    inportment delete respnse

--- index.js
+++ index.js
@@ -59,6 +59,15 @@ app.put('/api/course/:id', (req, res) => {
 
 });
 
+app.delete('/api/course/:id', (req, res) => {
+    const course = courses.find((c) => c.id === Number(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID not found.');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.send(course);
+})
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
